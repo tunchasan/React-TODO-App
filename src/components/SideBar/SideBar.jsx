0bd2c72639841eedbb8d7e0056426edf875a597f
tunchasan <hasantunc_ogr@outlook.com>
2020-08-10 00:47:05 +0300

    Fix overflow text issue in SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { CollectionTitle } from "../index";
+import { Dehaze } from "@material-ui/icons";
 import "./SiderBar.css";
 
 function SideBar() {
   return (
     <div className="sideBar">
       <div className="sideBar-inner">
-        <CollectionTitle title="Projectsfsdfdsfdsfdsfsdfsdfsdfsdfdssssssssssssssssssssssssssssssssss" />
+        <CollectionTitle title="Project" />
+      </div>
+      <div className="xx">
+        <h1>Inbox</h1>
+        <h1>Today</h1>
+        <h1>Upcoming</h1>
+        <h1>Mail</h1>
       </div>
     </div>
   );
