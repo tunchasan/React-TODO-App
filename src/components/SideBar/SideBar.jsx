7a7f346b9bbd94ae9ef95d1d7fcd98bc65208c3c
tunchasan <hasantunc_ogr@outlook.com>
2020-08-11 00:55:33 +0300

    Add floating-buttom to SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import "./SiderBar.css";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 import { CollectionHolder } from "../index";
 
 function SideBar() {
   return (
     <div className="sideBar_container">
+      <div className="addCollectionIcon_container">
+        <div className="addCollectionIcon">
+          <h1>+</h1>
+        </div>
+      </div>
+      <CollectionHolder
+        collectionTitle="Project"
+        collectionItemTitles={["React", "CSS", "Javascript"]}
+      />{" "}
+      <CollectionHolder
+        collectionTitle="Project"
+        collectionItemTitles={["React", "CSS", "Javascript"]}
+      />{" "}
+      <CollectionHolder
+        collectionTitle="Project"
+        collectionItemTitles={["React", "CSS", "Javascript"]}
+      />{" "}
+      <CollectionHolder
+        collectionTitle="Project"
+        collectionItemTitles={["React", "CSS", "Javascript"]}
+      />{" "}
+      <CollectionHolder
+        collectionTitle="Project"
+        collectionItemTitles={["React", "CSS", "Javascript"]}
+      />{" "}
+      <CollectionHolder
+        collectionTitle="Project"
+        collectionItemTitles={["React", "CSS", "Javascript"]}
+      />{" "}
       <CollectionHolder
         collectionTitle="Project"
         collectionItemTitles={["React", "CSS", "Javascript"]}
